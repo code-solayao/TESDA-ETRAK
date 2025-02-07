df235c72bd433272d31145f3e10d99da7e1e024e
Dave Andrew Solayao <102861448+code-solayao@users.noreply.github.com>
2025-02-07 16:43:24 +0800

    First Commit
    - Considering working on the project again but with PHP, Laravel this time

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TESDA - E-TRAK</title>
+</head>
+<body>
+    <div class="form-group">
+        <form>
+            <label for="username">Username</label>
+            <input type="text" name="username" id="username" class="form-control">
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" class="form-control">
+            <button type="submit" class="btn btn-primary">Login</button>
+        </form>
+    </div>
+</body>
+</html>
+<?php
+    echo "Hello World!";
+    echo "Hello World!";
+?>
